b8937e06c348463dd4fd68ef6e73a7315c60d5d3
David Alexandru <fanefaraon696@gmail.com>
2024-11-23 21:23:05 +0200

    added: database connection

---
+++ Infrastructure/DbContexts/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+}
---
+++ backend/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.DbContexts;
+
+namespace Presentation.Configuration
+{
+    public static class DatabaseConfiguration
+    {
+        public static IServiceCollection ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSqlServer<AppDbContext>(configuration.GetConnectionString("DefaultConnection"));
+
+            return services;
+        }
+    }
+}
